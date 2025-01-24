e672931b71ca5bcd496aa108aea37149eb00f155
SirYofat <siryofat@gmail.com>
2025-01-24 22:10:25 +0100

    feat: adds properties to the Border class
    This properties are booleans that return whereas a giben border pattern
    is in place:
    1. Corner
    2. Dead end
    3. Intersection

--- src/maze_solver/models/border.py
+++ src/maze_solver/models/border.py
@@ -6,3 +6,28 @@ class Border(IntFlag):
     BOTTOM = auto()
     LEFT = auto()
     RIGHT = auto()
+
+    @property
+    def corner(self) -> bool:
+        """bool: Returns if a square is a corner.
+
+        If a square has either combination of Top-Left/Right or Bottom-Left/Right,
+        it means it is a corner.
+        """
+        return self in (
+            self.TOP | self.LEFT,
+            self.TOP | self.RIGHT,
+            self.BOTTOM | self.LEFT,
+            self.BOTTOM | self.RIGHT,
+        )
+
+    @property
+    def dead_end(self) -> bool:
+        return self.bit_count() == 3
+
+    @property
+    def intersection(self) -> bool:
+        return self.bit_count() < 2
+
+border = Border(1)
+print(border.intersection)
