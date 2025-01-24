d5f4b60480ff5caf5e50e5914d47bfe542781f0e
SirYofat <siryofat@gmail.com>
2025-01-24 23:03:36 +0100

    feat: Makes Maze iterable
    Implements __iter__ and __getitem__

--- src/maze_solver/models/maze.py
+++ src/maze_solver/models/maze.py
@@ -1,7 +1,14 @@
 from dataclasses import dataclass
+from typing import Iterator
 
 from maze_solver.models.square import Square
 
 @dataclass(frozen=True)
 class Maze:
     squares: tuple[Square, ...] #It's a tuple and not a list to allow cacheing.
+
+    def __iter__(self) -> Iterator[Square]:
+        return iter(self.squares)
+
+    def __getitem__(self, index: int) -> Square:
+        return self.squares[index]
