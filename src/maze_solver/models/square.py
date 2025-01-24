081f71dfe5cb629b4efca8297a04325a6444bd4f
SirYofat <siryofat@gmail.com>
2025-01-24 22:58:06 +0100

    bug: removes colon after @dataclass decorator

--- src/maze_solver/models/square.py
+++ src/maze_solver/models/square.py
@@ -3,7 +3,7 @@ from dataclasses import dataclass
 from maze_solver.models.border import Border
 from maze_solver.models.role import Role
 
-@dataclass(frozen=True):
+@dataclass(frozen=True)
 class Square():
     index: int
     row: int
