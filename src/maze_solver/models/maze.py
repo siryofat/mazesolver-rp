4c79b5c45e249ca88698b6bc4672c41509f68098
SirYofat <siryofat@gmail.com>
2025-01-25 22:08:42 +0100

    feat: adds validation post init
    Implements a __post_init__ method to validate that the maze created has
    a series of squares with valid index, row and column.

--- src/maze_solver/models/maze.py
+++ src/maze_solver/models/maze.py
@@ -8,6 +8,10 @@ from maze_solver.models.square import Square
 class Maze:
     squares: tuple[Square, ...] #It's a tuple and not a list to allow cacheing.
 
+    def __post_init__(self) -> None:
+        validate_indices(self)
+        validate_rows_columns(self)
+
     def __iter__(self) -> Iterator[Square]:
         return iter(self.squares)
 
@@ -21,3 +25,19 @@ class Maze:
     @cached_property
     def height(self):
         return max(square.row for square in self) + 1
+
+
+def validate_indices(maze: Maze) -> None:
+    """Checks if index is a sequence of numbers up to the length of the Maze.
+    """
+    assert [square.index for square in maze ] == list(range(len(maze.squares))), "Wrong square.index"
+
+
+def validate_rows_columns(maze: Maze) -> None:
+    """Checks if each square has a valid row and column that matches an index.
+    """
+    for y in range(maze.height):
+        for x in range(maze.width):
+            square = maze[y * maze.width + x]
+            assert square.row == y, "Wrong square.row"
+            assert square.column == x, "Wrong square.column"
