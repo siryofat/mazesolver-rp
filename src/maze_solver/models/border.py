58f087008e35ec1343a3718e676cdff2c537f7c5
SirYofat <siryofat@gmail.com>
2025-01-25 22:32:38 +0100

    doc: adds docstring to border.

--- src/maze_solver/models/border.py
+++ src/maze_solver/models/border.py
@@ -23,11 +23,16 @@ class Border(IntFlag):
 
     @property
     def dead_end(self) -> bool:
+        """bool: Returns if a square is a dead end.
+
+        If a square has three sides, it is a dead end.
+        """
         return self.bit_count() == 3
 
     @property
     def intersection(self) -> bool:
-        return self.bit_count() < 2
+        """bool: Returns if a square is an intersection.
 
-border = Border(1)
-print(border.intersection)
+        If a square has only 1 or 0 sides, its and intersection (T or cross, respectively)
+        """
+        return self.bit_count() < 2
