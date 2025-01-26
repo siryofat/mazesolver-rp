caaf7885c0cb701207dd35a6a851fcd16c3c514f
SirYofat <siryofat@gmail.com>
2025-01-26 22:06:49 +0100

    feat: makes solution class iterable
    Implements iter, getitem and len.

--- src/maze_solver/models/solution.py
+++ src/maze_solver/models/solution.py
@@ -1,7 +1,17 @@
 from dataclasses import dataclass
+from typing import Iterator
 
 from maze_solver.models.square import Square
 
 @dataclass(frozen=True)
 class Solution:
     squares: tuple[Square, ...]
+
+    def __iter__(self) -> Iterator[Square]:
+        return iter(self.squares)
+
+    def __getitem__(self, index:int) -> Square:
+        return self.squares[index]
+
+    def __len__(self) -> int:
+        return len(self.squares)
