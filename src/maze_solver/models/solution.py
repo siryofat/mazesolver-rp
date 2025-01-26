a25376e086e30be57c61f2e9ea5f58689e7c093a
SirYofat <siryofat@gmail.com>
2025-01-26 22:13:28 +0100

    feat: adds post init validator
    Implements post init to validate that the first square is an entrance
    and the last square is an exit. Also checks that all secuncial squares
    are in the same row or column.

--- src/maze_solver/models/solution.py
+++ src/maze_solver/models/solution.py
@@ -1,11 +1,18 @@
 from dataclasses import dataclass
 from typing import Iterator
+from functools import reduce
 
 from maze_solver.models.square import Square
+from maze_solver.models.role import Role
 
 @dataclass(frozen=True)
 class Solution:
     squares: tuple[Square, ...]
+
+    def __post_init__(self) -> None:
+        assert self.squares[0].role is Role.ENTRANCE, "First square is not entrance."
+        assert self.squares[-1].role is Role.EXIT, "Last square is not exit."
+        reduce(validate_corridor, self.squares)
 
     def __iter__(self) -> Iterator[Square]:
         return iter(self.squares)
@@ -15,3 +22,11 @@ class Solution:
 
     def __len__(self) -> int:
         return len(self.squares)
+
+
+def validate_corridor(current: Square, following: Square) -> Square:
+    assert any([
+        current.row == following.row,
+        current.column == following.column
+    ]), "Secuencial squares must lie in the same row or column."
+    return following
