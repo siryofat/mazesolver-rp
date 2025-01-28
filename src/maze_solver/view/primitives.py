70dd6d299d5743455bf536f68a5fc7e02bd36190
SirYofat <siryofat@gmail.com>
2025-01-28 21:40:34 +0100

    bug: fix variable name in return statement
    There was a typo in the variable name in the return statement. fixed.

--- src/maze_solver/view/primitives.py
+++ src/maze_solver/view/primitives.py
@@ -3,5 +3,5 @@ def tag(name: str, value: str | None = None, **attributes) -> str:
         f'{key.replace("_", "-")}="{value}"' for key, value in attributes.items()
     )
     if value is None:
-        return f'<{name}{attributes} />'
-    return f'<{name}{attributes}>{value}</{name}>'
+        return f'<{name}{attrs} />'
+    return f'<{name}{attrs}>{value}</{name}>'
