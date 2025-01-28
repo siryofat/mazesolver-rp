269a5f42dcedd03b2bdca26693d03f81d3368c8a
SirYofat <siryofat@gmail.com>
2025-01-28 21:59:33 +0100

    feat: Implements Line Primitive
    Line returns a string with the tag format.

--- src/maze_solver/view/primitives.py
+++ src/maze_solver/view/primitives.py
@@ -15,6 +15,21 @@ class Point(NamedTuple):
     def translate(self, x=0, y=0) -> 'Point':
         return Point(self.x + x, self.y + y)
 
+
+class Line(NamedTuple):
+    start: Point
+    end: Point
+
+    def draw(self, **attributes):
+        return tag(
+            'line',
+            x1 = self.start.x,
+            y1 = self.start.y,
+            x2 = self.end.x,
+            y2 = self.end.y,
+            **attributes,
+        )
+
 def tag(name: str, value: str | None = None, **attributes) -> str:
     attrs = "" if not attributes else " " + " ".join(
         f'{key.replace("_", "-")}="{value}"' for key, value in attributes.items()
