82b123cd5609f2264b0bba2c4c803f1ee4082675
SirYofat <siryofat@gmail.com>
2025-01-28 21:47:41 +0100

    feat: creates Primitive class
    Primitive class inherits from Protocol.

--- src/maze_solver/view/primitives.py
+++ src/maze_solver/view/primitives.py
@@ -1,3 +1,10 @@
+from typing import Protocol
+
+class Primitive(Protocol):
+    def draw(self, **attributes) -> str:
+        ...
+
+
 def tag(name: str, value: str | None = None, **attributes) -> str:
     attrs = "" if not attributes else " " + " ".join(
         f'{key.replace("_", "-")}="{value}"' for key, value in attributes.items()
