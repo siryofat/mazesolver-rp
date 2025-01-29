81b9a746049906fab6b16efddc8fca76e0f8cfff
SirYofat <siryofat@gmail.com>
2025-01-29 21:56:03 +0100

    feat: logic to create two sided squares

--- src/maze_solver/view/decomposer.py
+++ src/maze_solver/view/decomposer.py
@@ -2,6 +2,7 @@ from maze_solver.models.border import Border
 from maze_solver.view.primitives import(
     Point,
     Line,
+    DisjointLines,
     Polygon,
     Polyline,
     Primitive,
@@ -66,3 +67,45 @@ def decompose(border: Border, top_left: Point, square_size: int) -> Primitive:
                 top_left,
             ]
         )
+
+    if border is Border.LEFT | Border.TOP:
+        return Polyline(
+            [
+                bottom_left,
+                top_left,
+                top_right,
+            ]
+        )
+
+    if border is Border.TOP | Border.RIGHT:
+        return Polyline(
+            [
+                top_left,
+                top_right,
+                bottom_right,
+            ]
+        )
+
+    if border is Border.BOTTOM | Border.LEFT:
+        return Polyline(
+            [
+                bottom_right,
+                bottom_left,
+                top_left,
+            ]
+        )
+
+    if border is Border.RIGHT | Border.BOTTOM:
+        return Polyline(
+            [
+                top_right,
+                bottom_right,
+                bottom_left,
+            ]
+        )
+
+    if border is Border.LEFT | Border.RIGHT:
+        return DisjointLines([left, right])
+
+    if border is Border.TOP | Border.BOTTOM:
+        return DisjointLines([top, bottom])
