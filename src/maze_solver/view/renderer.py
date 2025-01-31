5d3183210e322020445845c02372befb997f3ffe
SirYofat <siryofat@gmail.com>
2025-01-31 22:26:05 +0100

    feat: implements render method
    Calculates margins, width and height and pass that to tag().
    
    Pending to implement _get_body().

--- src/maze_solver/view/renderer.py
+++ src/maze_solver/view/renderer.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 
 from maze_solver.models.maze import Maze
 from maze_solver.models.solution import Solution
+from maze_solver.view.primitives import tag
 
 @dataclass(frozen=True)
 class SVG:
@@ -18,4 +19,17 @@ class SVGRenderer:
         self.line_width // 2
 
     def render(self, maze: Maze, solution: Solution | None = None) -> SVG:
-        ...
+        margins = 2 * (self.offset + self.line_width)
+        width = margins + maze.width * self.square_size
+        height = margins + maze.height * self.square_size
+        return SVG(
+            tag(
+                'svg',
+                self._get_body(maze, solution),
+                xmlns="http.www.w3.org/2000/svg"
+                stroke_linejoin="round",
+                width=width,
+                height=height,
+                viewBox=f'0 0 {width} {height}',
+            )
+        )
