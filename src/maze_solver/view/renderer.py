b1d6b80ed928500968e7f73b72a46df3cf840c80
SirYofat <siryofat@gmail.com>
2025-01-31 23:29:16 +0100

    feat: implements _draw_square logic
    _draw_square is a missing method for getting xml body.
    
    To draw a square, a _transform and _draw_border methods are required.
    Also, the square can have roles, so an exterior, wall or label functions
    are mapped to a square.Role property.

--- src/maze_solver/view/renderer.py
+++ src/maze_solver/view/renderer.py
@@ -2,7 +2,17 @@ from dataclasses import dataclass
 
 from maze_solver.models.maze import Maze
 from maze_solver.models.solution import Solution
-from maze_solver.view.primitives import tag, Rect
+from maze_solver.models.square import Square
+from maze_solver.view.primitives import tag, Rect, Point, Text
+from maze_solver.view.decomposer import decompose
+from maze_solver.models.role import Role
+
+ROLE_EMOJI = {
+    Role.ENTRANCE: "\N{pedestrian}",
+    Role.EXIT: "\N{chequered flag}",
+    Role.ENEMY: "\N{ghost}",
+    Role.REWARD: "\N{white medium star}",
+}
 
 @dataclass(frozen=True)
 class SVG:
@@ -41,6 +51,62 @@ class SVGRenderer:
             *map(self._draw_square, maze),
             self._draw_solution(solution) if solution else "",
         ])
+
+    def _transform(self, square: Square, extra_offset: int = 0) -> Point:
+        return Point(
+            x = square.column * self.square_size,
+            y = square.row * self.square_size,
+        ).translate(
+            x = self.offset + extra_offset,
+            y = self.offset + extra_offset
+        )
+
+    def _draw_square(self, square:Square) -> str:
+        top_left: Point = self._transform(square)
+        tags = []
+        if square.role is Role.EXTERIOR:
+            tags.append(exterior(top_left, self.square_size, self.line_width))
+        elif square.role is Role.WALL:
+            tags.append(wall(top_left, self.square_size, self.line_width))
+        elif emoji := ROLE_EMOJI.get(square.role):
+            tags.append(label(emoji, top_left, self.square_size // 2))
+        tags.append(self._draw_border(square, top_left))
+        return "".join(tags)
+
+    def _draw_border(self, square: Square, top_left: Point) -> str:
+        return decompose(square.border, top_left, self.square_size).draw(
+            stroke_width = self.line_width,
+            stroke = "black",
+            fill = "none",
+        )
+
+
+def exterior(top_left: Point, size: int, line_width: int) -> str:
+    return Rect(top_left).draw(
+        width=size,
+        height=size,
+        stroke_width=line_width,
+        stroke="none",
+        fill="white"
+    )
+
+
+def wall(top_left: Point, size: int, line_width: int) -> str:
+    return Rect(top_left).draw(
+        width=size,
+        height=size,
+        stroke_width=line_width,
+        stroke="none",
+        fill="lightgray"
+    )
+
+
+def label(emoji: str, top_left: Point, offset: int) -> str:
+    return Text(emoji, top_left.translate(x=offset, y=offset)).draw(
+        font_size=f"{offset}px",
+        text_anchor="middle",
+        dominant_baseline="middle"
+    )
 
 
 def arrow_marker() -> str:
