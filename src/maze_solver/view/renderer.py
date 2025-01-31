64a5a07c9412400887db581d1fdd5d19aa401572
SirYofat <siryofat@gmail.com>
2025-01-31 22:20:07 +0100

    feat: creates SVG class and render method
    Within SVGRenderer creates a render method that will return the newly
    created SVG class. SVG holds a string representation of the xml content.

--- src/maze_solver/view/renderer.py
+++ src/maze_solver/view/renderer.py
@@ -1,5 +1,13 @@
 from dataclasses import dataclass
 
+from maze_solver.models.maze import Maze
+from maze_solver.models.solution import Solution
+
+@dataclass(frozen=True)
+class SVG:
+    xml_content: str
+
+
 @dataclass(frozen=True)
 class SVGRenderer:
     square_size: int = 100
@@ -8,3 +16,6 @@ class SVGRenderer:
     @property
     def offset(self):
         self.line_width // 2
+
+    def render(self, maze: Maze, solution: Solution | None = None) -> SVG:
+        ...
